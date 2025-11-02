f7a793f9bb196041c06a5f9ea0cadda6e6ed48c4
brajesh995 <brajesh970865@gmail.com>
2025-11-02 22:00:23 +0530

    Update MongoDB URI in index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://pritiprabha:priti@123@cluster0.uy7m0kr.mongodb.net/birth';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://231fa04c59_db_user:Piyush2005@cluster0.mwoixjm.mongodb.net/birth?appName=Cluster0';
 
 app.use(express.json());
 app.use(cors({
